42aa832bd5334da3ccb2f4fc731af1b46cd7efba
Vaelzan <vaelzan@evilcult.net>
2024-05-06 08:40:18 +1000

    Disable Upgrade Aquatic Bedrolls.

--- kubejs/client_scripts/jei_blacklist.js
+++ kubejs/client_scripts/jei_blacklist.js
@@ -12,6 +12,7 @@
  */
 JEIEvents.hideItems(event => {
   event.hide([
+    /upgrade_aquatic:.*bedroll/,
     /valhelsia_structures:.*sleeping_bag/,
   ]);
 });
--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -9,11 +9,13 @@
  */
 ServerEvents.recipes(event => {
 
-  // Remove recipes for all VS sleeping bags.
+  // Remove recipes for all sleeping bags & bedrolls.
+  COLORS.forEach((color) => event.remove({id: `upgrade_aquatic:${color}_bedroll`}));
   COLORS.forEach((color) => event.remove({id: `valhelsia_structures:${color}_sleeping_bag`}));
 
   // Remove recipes by recipe ID:
   [
+    'supplementaries:soap_clean_upgrade_aquatic_bedroll',
     'supplementaries:soap_clean_valhelsia_structures_sleeping_bag',
   ].forEach((recipeID) => event.remove({id: recipeID}));
 });
