17ef9f4502f6882a716e09315f1ed1312b98aa31
Vaelzan <vaelzan@evilcult.net>
2024-05-06 10:14:22 +1000

    Add display name for pack (bth) + KubeJS namespaces. Fix Butcher's Delight display name.

---
+++ kubejs/startup_scripts/misc.js
@@ -0,0 +1,9 @@
+// priority: 90
+
+/**
+ * @file Misc startup script contents for Beyond the Horizon.
+ */
+
+Platform.getInfo('bth').name = 'Beyond the Horizon';
+Platform.mods.kubejs.name = 'Beyond the Horizon';
+Platform.mods.butchersdelight.name = 'Butcher\'s Delight'; // Fix missing punctuation and space.
