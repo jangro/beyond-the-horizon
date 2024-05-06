bf54d24d727ff517a5c69378050eda9ba9122047
Vaelzan <vaelzan@evilcult.net>
2024-05-06 11:38:36 +1000

    Limit Stray Tower + Ice Tower generation to snowy mountains.

--- kubejs/server_scripts/tags/biome.js
+++ kubejs/server_scripts/tags/biome.js
@@ -2,6 +2,8 @@
 
 /**
  * @file Biome Tag Modifications for Beyond the Horizon.
+ * 
+ * Documentation: https://kubejs.com/wiki/tutorials/tags
  */
 
 /**
@@ -9,24 +11,36 @@
  */
 ServerEvents.tags('worldgen/biome', event => {
 
-  // Only allow towers to spawn in mountains
-  event.get('totw_modded:has_structure/desert_tower').removeAll();
-  event.get('totw_modded:has_structure/ice_tower').removeAll();
-  event.add('totw_modded:has_structure/ice_tower', [
-    'minecraft:frozen_peak',
-  ]);
-  event.get('totw_modded:has_structure/derelict_tower').removeAll();
-  event.add('totw_modded:has_structure/derelict_tower', [
-    '#minecraft:is_mountain',
-  ]);
-  event.get('totw_modded:has_structure/derelict_grass_tower').removeAll();
-  event.get('totw_modded:has_structure/jungle_tower').removeAll();
-  event.get('totw_modded:has_structure/badlands_tower').removeAll();
-  event.get('totw_modded:has_structure/ocean_warm_tower').removeAll();
-  event.get('totw_modded:has_structure/ocean_tower').removeAll();
-  event.get('totw_modded:has_structure/regular_tower').removeAll();
-  event.add('totw_modded:has_structure/regular_tower', [
-    '#minecraft:is_mountain',
-  ]);
+  // Fill Snowy Moutain Biome tag.
+  const snowy_biomes = event.get('forge:is_snowy').getObjectIds();
+  const mountain_biomes = event.get('minecraft:is_mountain').getObjectIds();
+  snowy_biomes.forEach(snowyBiomeID => {
+    mountain_biomes.forEach(mountainBiomeID => {
+      if (snowyBiomeID == mountainBiomeID) {
+        event.add('bth:is_snowy_mountain', mountainBiomeID);
+      }
+    });
+  });
+
+  // Eidolon - Stray Towers only in snowy mountains.
+  event.removeAll('eidolon:has_structure/stray_tower');
+  event.add('eidolon:has_structure/stray_tower', '#bth:is_snowy_mountain');
+
+  // TOTW - Only allow towers to spawn in mountains.
+  event.removeAll('totw_modded:has_structure/desert_tower');
+  event.removeAll('totw_modded:has_structure/derelict_grass_tower');
+
+  event.removeAll('totw_modded:has_structure/ice_tower');
+  event.add('totw_modded:has_structure/ice_tower', '#bth:is_snowy_mountain');
+  
+  event.removeAll('totw_modded:has_structure/derelict_tower');
+  event.add('totw_modded:has_structure/derelict_tower', '#minecraft:is_mountain');
+  event.removeAll('totw_modded:has_structure/jungle_tower');
+  event.removeAll('totw_modded:has_structure/badlands_tower');
+  event.removeAll('totw_modded:has_structure/ocean_warm_tower');
+  event.removeAll('totw_modded:has_structure/ocean_tower');
+
+  event.removeAll('totw_modded:has_structure/regular_tower');
+  event.add('totw_modded:has_structure/regular_tower', '#minecraft:is_mountain');
 
 });
