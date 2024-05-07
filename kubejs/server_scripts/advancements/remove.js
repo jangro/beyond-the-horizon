94e4e49dc3352a9a722982b2cee33aa88af95a2a
Vaelzan <vaelzan@evilcult.net>
2024-05-07 20:31:10 +1000

    Prevent mods from granting manuals on first join (replaced with Eccentric Tome).

---
+++ kubejs/server_scripts/advancements/remove.js
@@ -0,0 +1,32 @@
+// priority: 15
+
+/**
+ * @file Removes unused / undesired advancements from the pack.
+ */
+
+/**
+ * Which advancements to remove from the pack.
+ * These should be the resource location of the advancement, including the .json file extension.
+ * @example Example entry: 'irons_spellbooks:advancements/grant_patchouli.json'
+ * @const {!string[]}
+ */
+const ADVANCEMENTS_TO_REMOVE = [
+  'irons_spellbooks:advancements/grant_patchouli.json',
+];
+
+/**
+ * Empty advancement JSON, for the purposes of effectively deleting an advancement (but without triggering errors).
+ * @const {!Object}
+ */
+const EMPTY_ADVANCEMENT = {criteria:{impossible:{trigger:'minecraft:impossible'}}};
+
+/**
+ * Event handler for KubeJS's virtual datapack. Removes unused / undesired advancements
+ * from the pack.
+ */
+ServerEvents.highPriorityData(event => {
+  // Remove specified advancements.
+  ADVANCEMENTS_TO_REMOVE.forEach(file => {
+    event.addJson(file, EMPTY_ADVANCEMENT);
+  });
+});
