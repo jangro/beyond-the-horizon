451c8baa3f216fa33093e143236d084acf170d8f
Vaelzan <vaelzan@evilcult.net>
2024-05-07 13:21:14 +1000

    Add filled Eccentric Tome + recipe, first start, and JEI information for it.

---
+++ kubejs/client_scripts/jei_information.js
@@ -0,0 +1,32 @@
+// priority: 0
+
+/**
+ * @file JEI Information for Beyond the Horizon.
+ * 
+ */
+
+/**
+ * Event handler for adding JEI information.
+ */
+JEIEvents.information(event => {
+
+  /**
+   * Adds translated JEI information pane(s) to one or more items.
+   * @param {string|string[]} items The ID(s) of the item(s) to add.
+   * @param {string|string[]} textID The language entry/entries for the information.
+   */
+  const addInfo = (items, textID) => {
+    if (Array.isArray(textID)) {
+      textID.forEach((id) => event.addItem(items, Text.translate(id)));
+    } else {
+      event.addItem(items, Text.translate(textID));
+    }
+  };
+
+  // Minecraft
+
+  // Eccentric Tome
+  addInfo('eccentrictome:tome', 'bth.information.eccentrictome.filled_eccentric_tome');
+  addInfo('eccentrictome:tome', 'bth.information.eccentrictome.reverting');
+
+});
--- kubejs/server_scripts/recipes/shaped.js
+++ kubejs/server_scripts/recipes/shaped.js
@@ -2,6 +2,12 @@
 
 /**
  * @file Shaped recipe additions for Beyond the Horizon.
+ * 
+ * Note: Recipes should be in this order:
+ * 1) Beyond the Horizon (pack-specific content)
+ * 2) Minecraft (vanilla)
+ * 3) Other mods, alphabetically.
+ * 
  */
 
 /**
@@ -10,6 +16,17 @@
 ServerEvents.recipes(event => {
   const ID_PREFIX = 'bth:crafting/'; // Recipe ID
 
+  // Beyond the Horizon
+  event.shaped(Item.of('eccentrictome:tome', ECCENTRIC_TOME_NBT), [
+    ' S ',
+    'SBS',
+    ' S '
+  ], {
+    S: '#forge:bookshelves',
+    B: 'minecraft:book'
+  }).id(`${ID_PREFIX}filled_eccentric_tome`);
+
+  // Minecraft
   event.shaped('minecraft:saddle', [
     'SLS',
     'ILI'
@@ -18,4 +35,6 @@ ServerEvents.recipes(event => {
     L: '#forge:leather',
     I: '#forge:ingots/iron'
   }).id(`${ID_PREFIX}saddle`);
+
+
 });
