dab482310aeac544100853d6fe3a0c83669f6694
Johannes Lundberg <johalun0@gmail.com>
2024-05-08 12:28:17 -0700

    Remove wardrobe mod's backpack recipes

--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -17,5 +17,8 @@ ServerEvents.recipes(event => {
   [
     'supplementaries:soap_clean_upgrade_aquatic_bedroll',
     'supplementaries:soap_clean_valhelsia_structures_sleeping_bag',
+    'wardrobe:backpack',
+    'wardrobe:large_backpack',
+    'wardrobe:travel_backpack'
   ].forEach((recipeID) => event.remove({id: recipeID}));
 });
