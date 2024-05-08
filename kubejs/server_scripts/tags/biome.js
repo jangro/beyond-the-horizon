45a234cbbcd2a75d4d79b72d8b3ad63e3ec2952c
Vaelzan <vaelzan@evilcult.net>
2024-05-08 21:35:32 +1000

    Cleanup biome.js.

--- kubejs/server_scripts/tags/biome.js
+++ kubejs/server_scripts/tags/biome.js
@@ -22,48 +22,50 @@ ServerEvents.tags('worldgen/biome', event => {
     });
   });
 
-  // Eidolon - Stray Towers only in snowy mountains.
-  event.removeAll('eidolon:has_structure/stray_tower_biomes');
+  // Remove these structures from all biomes:
+  [
+    'dungeons_arise:has_structure/heavenly_challenger_biomes',
+    'dungeons_arise:has_structure/heavenly_conqueror_biomes',
+    'dungeons_arise:has_structure/heavenly_rider_biomes',
+    'dungeons_arise:has_structure/illager_corsair_biomes',
+    'dungeons_arise:has_structure/illager_galley_biomes',
+    'dungeons_arise:has_structure/mechanical_nest_biomes',
+    'dungeons_arise:has_structure/mushroom_house_biomes',
+    'dungeons_arise:has_structure/mushroom_mines_biomes',
+    'dungeons_arise:has_structure/scorched_mines_biomes',
+    'dungeons_arise:has_structure/small_blimp_biomes',
+    'dungeons_arise:has_structure/wishing_well_biomes',
+
+    'eidolon:has_structure/stray_tower_biomes',
+    
+    'minecolonies:has_structure/asian_colony',
+    'minecolonies:has_structure/caledonia_colony',
+    'minecolonies:has_structure/colonial_colony',
+    'minecolonies:has_structure/desertoasis_colony',
+    'minecolonies:has_structure/incan_colony',
+    'minecolonies:has_structure/lostmesacity_colony',
+    'minecolonies:has_structure/medieval_birch_colony',
+    'minecolonies:has_structure/medieval_dark_oak_colony',
+    'minecolonies:has_structure/medieval_oak_colony',
+    'minecolonies:has_structure/medieval_spruce_colony',
+    'minecolonies:has_structure/shire_colony',
+    'minecolonies:has_structure/warpednetherlands_colony',
+
+    'totw_modded:has_structure/badlands_tower',
+    'totw_modded:has_structure/derelict_tower',
+    'totw_modded:has_structure/derelict_grass_tower',
+    'totw_modded:has_structure/desert_tower',
+    'totw_modded:has_structure/ice_tower',
+    'totw_modded:has_structure/jungle_tower',
+    'totw_modded:has_structure/ocean_tower',
+    'totw_modded:has_structure/ocean_warm_tower',
+    'totw_modded:has_structure/regular_tower',
+  ].forEach((tag) => event.removeAll(tag));
+
+  // Eidolon - Stray Towers spawn only in snowy mountains.
   event.add('eidolon:has_structure/stray_tower_biomes', '#bth:is_snowy_mountain');
 
-  // TOTW - Only allow towers to spawn in mountains, and disable the broken 'derelict' towers.
-  event.removeAll('totw_modded:has_structure/badlands_tower');
-  event.removeAll('totw_modded:has_structure/derelict_tower');
-  event.removeAll('totw_modded:has_structure/derelict_grass_tower');
-  event.removeAll('totw_modded:has_structure/desert_tower');
-  event.removeAll('totw_modded:has_structure/ice_tower');
+  // TOTW - Regular Towers spawn only in mountains + Ice Towers in snowy mountains.
   event.add('totw_modded:has_structure/ice_tower', '#bth:is_snowy_mountain');
-  event.removeAll('totw_modded:has_structure/jungle_tower');
-  event.removeAll('totw_modded:has_structure/ocean_tower');
-  event.removeAll('totw_modded:has_structure/ocean_warm_tower');
-  event.removeAll('totw_modded:has_structure/regular_tower');
   event.add('totw_modded:has_structure/regular_tower', '#minecraft:is_mountain');
-
-  // Remove unwanted When Dungeons Arise structures
-  event.removeAll('dungeons_arise:has_structure/heavenly_challenger_biomes');
-  event.removeAll('dungeons_arise:has_structure/heavenly_conqueror_biomes');
-  event.removeAll('dungeons_arise:has_structure/heavenly_rider_biomes');
-  event.removeAll('dungeons_arise:has_structure/illager_corsair_biomes');
-  event.removeAll('dungeons_arise:has_structure/illager_galley_biomes');
-  event.removeAll('dungeons_arise:has_structure/mechanical_nest_biomes');
-  event.removeAll('dungeons_arise:has_structure/mushroom_house_biomes');
-  event.removeAll('dungeons_arise:has_structure/mushroom_mines_biomes');
-  event.removeAll('dungeons_arise:has_structure/scorched_mines_biomes');
-  event.removeAll('dungeons_arise:has_structure/small_blimp_biomes');
-  event.removeAll('dungeons_arise:has_structure/wishing_well_biomes');
-
-  // Disable empty minecolonies
-  event.removeAll('minecolonies:has_structure/asian_colony');
-  event.removeAll('minecolonies:has_structure/caledonia_colony');
-  event.removeAll('minecolonies:has_structure/colonial_colony');
-  event.removeAll('minecolonies:has_structure/desertoasis_colony');
-  event.removeAll('minecolonies:has_structure/incan_colony');
-  event.removeAll('minecolonies:has_structure/lostmesacity_colony');
-  event.removeAll('minecolonies:has_structure/medieval_birch_colony');
-  event.removeAll('minecolonies:has_structure/medieval_dark_oak_colony');
-  event.removeAll('minecolonies:has_structure/medieval_oak_colony');
-  event.removeAll('minecolonies:has_structure/medieval_spruce_colony');
-  event.removeAll('minecolonies:has_structure/shire_colony');
-  event.removeAll('minecolonies:has_structure/warpednetherlands_colony');
-
 });
