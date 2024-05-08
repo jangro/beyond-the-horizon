d597fd4536055751c117a34cc7926d9571acb93a
Vaelzan <vaelzan@evilcult.net>
2024-05-09 00:48:19 +1000

    Add unfinished command + info scripts for future use.

---
+++ kubejs/server_scripts/dev/info.js
@@ -0,0 +1,14 @@
+// priority: 25
+
+/**
+ * @file Functions to export information to disk to aid in pack development.
+ */
+
+/**
+ * Exports a list of all entities in the pack to a file.
+ * @returns The number of entities.
+ */
+function export_entity_info(ctx) {
+  // TODO
+  return 0;
+}
