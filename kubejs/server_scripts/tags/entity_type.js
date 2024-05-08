dce7c11918948e5b41eb6192e244dc45f57bb5c3
Vaelzan <vaelzan@evilcult.net>
2024-05-09 00:51:05 +1000

    Blacklist Mimic and Dead King from Drygmies. Add Magic Find tag to several mobs.

---
+++ kubejs/server_scripts/tags/entity_type.js
@@ -0,0 +1,31 @@
+// priority: 110                   
+
+/**
+ * @file Entity Type Tag additions for Beyond the Horizon.
+ */
+
+/**
+ * Entity Type Tag Event Handler
+ */
+ServerEvents.tags('entity_type', event => {
+
+  // Blacklist some overpowered drygmy mobs (mainly bosses).
+  event.add('ars_nouveau:drygmy_blacklist', [
+    'artifacts:mimic',
+    'irons_spellbooks:dead_king',
+    // TODO: Some other Iron's entities should probably be added here.
+  ]);
+
+  // Expand Ars Nouveau Magic Find to other mobs.
+  event.add('ars_nouveau:magic_find', [
+    'artifacts:mimic',
+    'irons_spellbooks:archevoker',
+    'irons_spellbooks:cryomancer',
+    'irons_spellbooks:dead_king',
+    'irons_spellbooks:necromancer',
+    'irons_spellbooks:priest',
+    'irons_spellbooks:pyromancer',
+    'minecraft:evoker',
+    'minecraft:illusioner',
+  ]);
+});
