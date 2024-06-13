adec486b9423549a8d2c9239b65c81a0b2c6f7a2
Ollarion <155204787+Ollarion@users.noreply.github.com>
2024-06-13 20:24:10 +0100

    Allow more items to be inserted into Spellcaster Bag

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -73,4 +73,62 @@ ServerEvents.tags('item', event => {
     'forbidden_arcanus:deorum_block',
   ]);
 
+  // Ars Elemental Tags
+  event.add('ars_elemental:caster_bag_item', [
+    'aether:cloud_staff',
+    
+    'alexscaves:sea_staff',
+    
+    'ars_nouveau:enchanters_eye',
+    'ars_nouveau:enchanters_mirror',
+    'ars_nouveau:enchanters_sword',
+    'ars_nouveau:spell_bow',
+
+    'ars_nouveau:wand',
+    'ars_nouveau:hood',
+    'ars_nouveau:robe',
+    'ars_nouveau:legs',
+    'ars_nouveau:boot',
+
+    'ars_elemental:air_hat',
+    'ars_elemental:air_robes',
+    'ars_elemental:air_leggings',
+    'ars_elemental:air_boots',
+    'ars_elemental:aqua_hat',
+    'ars_elemental:aqua_robes',
+    'ars_elemental:aqua_leggings',
+    'ars_elemental:aqua_boots',
+    'ars_elemental:earth_hat',
+    'ars_elemental:earth_robes',
+    'ars_elemental:earth_leggings',
+    'ars_elemental:earth_boots',
+    'ars_elemental:fire_hat',
+    'ars_elemental:fire_robes',
+    'ars_elemental:fire_leggings',
+    'ars_elemental:fire_boots',
+
+    'ars_elemental:spell_crossbow',
+    'ars_elemental:spell_horn',
+    
+    'botania:rods',
+    
+    'eidolon:bonechill_wand',
+    'eidolon:soulfire_wand',
+    'eidolon:warlock_hat',
+    'eidolon:warlock_cloak',
+    'eidolon:warlock_boots',
+    
+    'irons_spellbooks:artificer_cane',
+    'irons_spellbooks:blood_staff',
+    'irons_spellbooks:graybeard_staff',
+    'irons_spellbooks:hither_thither_wand',
+    'irons_spellbooks:ice_staff',
+    'irons_spellbooks:scroll',
+    /irons_spellbooks:.*_spell_book$/,
+    /irons_spellbooks:.*_helmet$/,
+    /irons_spellbooks:.*_chestplate$/,
+    /irons_spellbooks:.*_leggings$/,
+    /irons_spellbooks:.*_boots$/,
+
+  ]);
 });
