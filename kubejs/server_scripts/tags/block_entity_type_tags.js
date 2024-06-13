c24c022345588a94cc06307733a8af6e9712dd8c
Vaelzan <vaelzan@evilcult.net>
2024-06-14 05:54:47 +1000

    Fix indentation in block_entity_type_tags.js + add comments.

--- kubejs/server_scripts/tags/block_entity_type_tags.js
+++ kubejs/server_scripts/tags/block_entity_type_tags.js
@@ -2,9 +2,23 @@
 
 /**
  * @file Block Entity Type Tag additions for Beyond the Horizon.
+ * 
+ * The order of entries here should be:
+ *   BTH Tags (if needed)
+ *   Minecraft Tags
+ *   Forge Tags
+ *   All other mods, alphabetically.
  */
 
- ServerEvents.tags('block_entity_type', event => {
-  // whitelist spawners for packing tape
-  event.add('packingtape:te_whitelist', ['minecraft:mob_spawner']);
- });
+ServerEvents.tags('block_entity_type', event => {
+  // Beyond the Horizon Tags
+
+  // Minecraft Tags
+
+  // Forge Tags
+
+  // Packing Tape Tags
+  event.add('packingtape:te_whitelist', [
+    'minecraft:mob_spawner',
+  ]);
+});
