7c1f773cac0742835ffd6313b579ecb9f961cf3e
Johannes Lundberg <johalun0@gmail.com>
2024-06-28 17:31:39 -0700

    Revert "Tag sourdough bread with #forge:bread"
    This reverts commit acdeec9a039cdd36293c670d13ee1e299ce1477a.

--- kubejs/server_scripts/tags/item.js
+++ kubejs/server_scripts/tags/item.js
@@ -24,12 +24,10 @@ ServerEvents.tags('item', event => {
   // Forge Tags
   event.add('forge:dough', [
     'minecolonies:bread_dough',
-    'pneumaticcraft:sourdough'
   ]);
 
   event.add('forge:dough/wheat', [
     'minecolonies:bread_dough',
-    'pneumaticcraft:sourdough',
   ]);
   
   event.add('forge:dusts', [
