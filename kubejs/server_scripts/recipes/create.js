e92b0de38dd37788cb1001ac479857b79240ea00
Johannes Lundberg <johalun0@gmail.com>
2024-08-03 13:40:17 -0700

    Add compacting recipe raw plastic -> plastic sheet

--- kubejs/server_scripts/recipes/create.js
+++ kubejs/server_scripts/recipes/create.js
@@ -15,12 +15,15 @@ ServerEvents.recipes(event => {
   event.recipes.create.mixing([Fluid.of('minecraft:milk 250')], [Fluid.of('minecraft:water 250'), '#bth:nuts']).heated().id(`${ID_PREFIX}mixing/milk_from_nuts`);
 
   // Crushing
-  event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+  event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
 
   // Milling
-  event.recipes.create.milling([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+  event.recipes.create.milling([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
 
   // Splashing / Washing
-  event.recipes.create.splashing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+  event.recipes.create.splashing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
+
+  // Compacting
+  event.recipes.create.compacting('pneumaticcraft:plastic', '3x rats:raw_plastic');
 
 });
