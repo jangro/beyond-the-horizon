198e6072eae07c0f24cf1a2171c3c0c8624a1c5c
Johannes Lundberg <johalun0@gmail.com>
2024-08-03 13:32:11 -0700

    Add Create recipes for plastic waste from garbage piles

--- kubejs/server_scripts/recipes/create.js
+++ kubejs/server_scripts/recipes/create.js
@@ -14,4 +14,13 @@ ServerEvents.recipes(event => {
   // Mixing
   event.recipes.create.mixing([Fluid.of('minecraft:milk 250')], [Fluid.of('minecraft:water 250'), '#bth:nuts']).heated().id(`${ID_PREFIX}mixing/milk_from_nuts`);
 
+  // Crushing
+  event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+
+  // Milling
+  event.recipes.create.milling([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+
+  // Splashing / Washing
+  event.recipes.create.splashing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile')
+
 });
