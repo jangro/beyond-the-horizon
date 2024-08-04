312ac8d194a9b7cfa224aad50bccc5660569e3d3
Johannes Lundberg <johalun0@gmail.com>
2024-08-04 11:14:43 -0700

    Add Terralith villages to way sign

--- kubejs/server_scripts/tags/structure.js
+++ kubejs/server_scripts/tags/structure.js
@@ -37,6 +37,7 @@ ServerEvents.tags('worldgen/structure', event => {
     'bth_structures:tinkerer_house',
     'bth_structures:magician_house',
     '#ctov:village',
+    '#terralith:fortified_villages',
     '#towns_and_towers:town',
   ]);
 
