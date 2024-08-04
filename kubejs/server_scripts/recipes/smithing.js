ea86dd3ceaee98c1ff2460a3a49ce07e99e10f51
Johannes Lundberg <johalun0@gmail.com>
2024-08-04 11:33:47 -0700

    Add smithing recipe for gavel

---
+++ kubejs/server_scripts/recipes/smithing.js
@@ -0,0 +1,19 @@
+// priority: 10
+
+/**
+ * @file Smithing recipe additions for Beyond the Horizon.
+ */
+
+/**
+ * Smithing Recipe Event Handler
+ */
+ServerEvents.recipes(event => {
+
+  event.smithing(
+    'forbidden_arcanus:netherite_blacksmith_gavel',
+    'minecraft:netherite_upgrade_smithing_template',
+    'forbidden_arcanus:diamond_blacksmith_gavel',
+    'minecraft:netherite_ingot'
+  )
+
+});
