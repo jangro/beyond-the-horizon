f244afa2930c944204901884efc5bc43724d2191
Johannes Lundberg <johalun0@gmail.com>
2024-08-04 10:06:59 -0700

    Add recipe IDs and make compacting plastic require heat

--- kubejs/server_scripts/recipes/create.js
+++ kubejs/server_scripts/recipes/create.js
@@ -11,19 +11,19 @@
 ServerEvents.recipes(event => {
   const ID_PREFIX = 'bth:create/';
 
+  // Compacting
+  event.recipes.create.compacting('pneumaticcraft:plastic', '3x rats:raw_plastic').heated().id(`${ID_PREFIX}compacting/plastic_from_raw_plastic`);
+
+  // Crushing
+  event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile').id(`${ID_PREFIX}crushing/plastic_waste_from_garbage_pile`);
+
+  // Milling
+  event.recipes.create.milling([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile').id(`${ID_PREFIX}milling/plastic_waste_from_garbage_pile`);
+
   // Mixing
   event.recipes.create.mixing([Fluid.of('minecraft:milk 250')], [Fluid.of('minecraft:water 250'), '#bth:nuts']).heated().id(`${ID_PREFIX}mixing/milk_from_nuts`);
 
-  // Crushing
-  event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
-
-  // Milling
-  event.recipes.create.milling([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
-
-  // Splashing / Washing
-  event.recipes.create.splashing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile');
-
-  // Compacting
-  event.recipes.create.compacting('pneumaticcraft:plastic', '3x rats:raw_plastic');
+  // Splashing
+  event.recipes.create.splashing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile').id(`${ID_PREFIX}splashing/plastic_waste_from_garbage_pile`);
 
 });
