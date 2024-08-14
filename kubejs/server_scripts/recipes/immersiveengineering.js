4a3dfbdfd21596c5e268d8ad3849c2defc331c01
Vaelzan <vaelzan@evilcult.net>
2024-08-14 15:30:14 +1000

    Add Hammer Crushing recipe for Diamond Grit + cleanup IE script.

--- kubejs/server_scripts/recipes/immersiveengineering.js
+++ kubejs/server_scripts/recipes/immersiveengineering.js
@@ -11,13 +11,7 @@
 ServerEvents.recipes(event => {
   const ID_PREFIX = 'bth:immersiveengineering/';
 
-  // Not an important recipe, just for testing functionality
-  event.recipes.immersiveengineering.metal_press(
-    'upgrade_aquatic:prismarine_rod',
-    'minecraft:prismarine_shard',
-    'immersiveengineering:mold_rod'
-  ).id(`${ID_PREFIX}metalpress/prismarine_rod`);
-
+  // Metal Press Recipes - Plates / Sheets
   event.recipes.immersiveengineering.metal_press(
     'createdeco:andesite_sheet',
     'create:andesite_alloy',
@@ -35,42 +29,43 @@ ServerEvents.recipes(event => {
     '#forge:ingots/netherite',
     'immersiveengineering:mold_plate'
   ).id(`${ID_PREFIX}metalpress/netherite_sheet`);
-  
-  // Added for BTH to match Create's recipe
-  event.recipes.immersiveengineering.crusher(
-    'createaddition:diamond_grit',
-    'minecraft:diamond'
-  ).id(`${ID_PREFIX}crusher/diamond_grit`);
 
+  // Metal Press Recipes - Rods
+  event.recipes.immersiveengineering.metal_press(
+    'upgrade_aquatic:prismarine_rod',
+    'minecraft:prismarine_shard',
+    'immersiveengineering:mold_rod'
+  ).id(`${ID_PREFIX}metalpress/prismarine_rod`);
+
+  // Crusher Recipes
   // Replace IE recipes to switch grit/dust for Create crushed ores
   ['aluminum', 'copper', 'gold', 'iron', 'lead', 'nickel', 'silver'].forEach((material) =>
-    {
-      event.remove({id: `immersiveengineering:crusher/ingot_${material}`});
-      event.remove({id: `immersiveengineering:crusher/ore_${material}`});
-      event.remove({id: `immersiveengineering:crusher/raw_block_${material}`});
-      event.remove({id: `immersiveengineering:crusher/raw_ore_${material}`});
+  {
+    event.remove({id: `immersiveengineering:crusher/ingot_${material}`});
+    event.remove({id: `immersiveengineering:crusher/ore_${material}`});
+    event.remove({id: `immersiveengineering:crusher/raw_block_${material}`});
+    event.remove({id: `immersiveengineering:crusher/raw_ore_${material}`});
 
-      event.recipes.immersiveengineering.crusher(
-        `create:crushed_raw_${material}`,
-        `#forge:ingots/${material}`
-      ).id(`${ID_PREFIX}crusher/ingot_${material}`);
+    event.recipes.immersiveengineering.crusher(
+      `create:crushed_raw_${material}`,
+      `#forge:ingots/${material}`
+    ).id(`${ID_PREFIX}crusher/ingot_${material}`);
 
-      event.recipes.immersiveengineering.crusher(
-        `2x create:crushed_raw_${material}`,
-        `#forge:ores/${material}`
-      ).id(`${ID_PREFIX}crusher/ore_${material}`);
+    event.recipes.immersiveengineering.crusher(
+      `2x create:crushed_raw_${material}`,
+      `#forge:ores/${material}`
+    ).id(`${ID_PREFIX}crusher/ore_${material}`);
 
-      event.recipes.immersiveengineering.crusher(
-        `12x create:crushed_raw_${material}`,
-        `#forge:storage_blocks/raw_${material}`
-      ).id(`${ID_PREFIX}crusher/raw_block_${material}`);
+    event.recipes.immersiveengineering.crusher(
+      `12x create:crushed_raw_${material}`,
+      `#forge:storage_blocks/raw_${material}`
+    ).id(`${ID_PREFIX}crusher/raw_block_${material}`);
 
-      event.recipes.immersiveengineering.crusher(
-        `create:crushed_raw_${material}`,
-        `#forge:raw_materials/${material}`
-      ).id(`${ID_PREFIX}crusher/raw_ore_${material}`);
-    }
-  );
+    event.recipes.immersiveengineering.crusher(
+      `create:crushed_raw_${material}`,
+      `#forge:raw_materials/${material}`
+    ).id(`${ID_PREFIX}crusher/raw_ore_${material}`);
+  });
 
   event.remove({id: 'immersiveengineering:crusher/nether_gold'});
   event.recipes.immersiveengineering.crusher(
@@ -78,16 +73,17 @@ ServerEvents.recipes(event => {
     'minecraft:nether_gold_ore'
   ).id(`${ID_PREFIX}crusher/nether_gold`);
 
-  // AE2 dusts from crusher
-  event.recipes.immersiveengineering.crusher('ae2:certus_quartz_dust', '#ae2:all_certus_quartz').id(`${ID_PREFIX}crusher/certus_quartz`);
-  event.recipes.immersiveengineering.crusher('ae2:ender_dust', 'minecraft:ender_pearl').id(`${ID_PREFIX}crusher/ender_pearl`);
-  event.recipes.immersiveengineering.crusher('ae2:fluix_dust', 'ae2:fluix_crystal').id(`${ID_PREFIX}crusher/fluix_crystal`);
-  event.recipes.immersiveengineering.crusher('ae2:sky_dust', 'ae2:sky_stone_block').id(`${ID_PREFIX}crusher/sky_stone_block`);
+  event.recipes.immersiveengineering.crusher('ae2:certus_quartz_dust', '#ae2:all_certus_quartz').id(`${ID_PREFIX}crusher/certus_quartz_dust`);
+  event.recipes.immersiveengineering.crusher('ae2:ender_dust', 'minecraft:ender_pearl').id(`${ID_PREFIX}crusher/ender_dust`);
+  event.recipes.immersiveengineering.crusher('ae2:fluix_dust', 'ae2:fluix_crystal').id(`${ID_PREFIX}crusher/fluix_dust`);
+  event.recipes.immersiveengineering.crusher('ae2:sky_dust', 'ae2:sky_stone_block').id(`${ID_PREFIX}crusher/sky_stone_dust`);
+  event.recipes.immersiveengineering.crusher('createaddition:diamond_grit', '#forge:gems/diamond').id(`${ID_PREFIX}crusher/diamond_grit`);
 
-  // AE2 dusts from hammering
-  event.recipes.immersiveengineering.hammer_crushing('ae2:certus_quartz_dust', '#ae2:all_certus_quartz').id(`${ID_PREFIX}hammer_crushing/certus_quartz`);
-  event.recipes.immersiveengineering.hammer_crushing('ae2:ender_dust', 'minecraft:ender_pearl').id(`${ID_PREFIX}hammer_crushing/ender_pearl`);
-  event.recipes.immersiveengineering.hammer_crushing('ae2:fluix_dust', 'ae2:fluix_crystal').id(`${ID_PREFIX}hammer_crushing/fluix_crystal`);
-  event.recipes.immersiveengineering.hammer_crushing('ae2:sky_dust', 'ae2:sky_stone_block').id(`${ID_PREFIX}hammer_crushing/sky_stone_block`);
+  // Hammer Crushing Recipes
+  event.recipes.immersiveengineering.hammer_crushing('ae2:certus_quartz_dust', '#ae2:all_certus_quartz').id(`${ID_PREFIX}hammer_crushing/certus_quartz_dust`);
+  event.recipes.immersiveengineering.hammer_crushing('ae2:ender_dust', 'minecraft:ender_pearl').id(`${ID_PREFIX}hammer_crushing/ender_dust`);
+  event.recipes.immersiveengineering.hammer_crushing('ae2:fluix_dust', 'ae2:fluix_crystal').id(`${ID_PREFIX}hammer_crushing/fluix_dust`);
+  event.recipes.immersiveengineering.hammer_crushing('ae2:sky_dust', 'ae2:sky_stone_block').id(`${ID_PREFIX}hammer_crushing/sky_stone_dust`);
+  event.recipes.immersiveengineering.hammer_crushing('createaddition:diamond_grit', '#forge:gems/diamond').id(`${ID_PREFIX}hammer_crushing/diamond`);
 
 });
