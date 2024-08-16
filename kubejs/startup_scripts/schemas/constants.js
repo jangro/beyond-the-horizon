adca25531491fa8b6752096a04649602564171ed
Vaelzan <vaelzan@evilcult.net>
2024-08-16 21:36:54 +1000

    Add new classes to constants.js for future use.

--- kubejs/startup_scripts/schemas/constants.js
+++ kubejs/startup_scripts/schemas/constants.js
@@ -6,6 +6,8 @@
  * Note: A '$' prefix is a naming convention that indicates that a constant is a Java class.
  */
 
+const $MapRecipeComponent = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.MapRecipeComponent');
 const $RecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.RecipeSchema');
 const $RecipeComponentBuilder = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.RecipeComponentBuilder');
+const $RecipeComponentBuilderMap = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.RecipeComponentBuilderMap');
 const $ShapedRecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.minecraft.ShapedRecipeSchema');
