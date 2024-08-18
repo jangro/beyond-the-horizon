2c95d419016913add462c21680a795553606fcbb
Vaelzan <vaelzan@evilcult.net>
2024-08-19 02:18:13 +1000

    Fix version detection in build_pack.bat

--- build_pack.bat
+++ build_pack.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Read the pack.toml file and extract the version
-for /f "tokens=2 delims== " %%a in ('findstr /i "version" pack.toml') do (
+for /f "tokens=2 delims== " %%a in ('findstr /i /b "version" pack.toml') do (
     set version=%%a
     set version=!version:"=!
     set version=!version: =!
