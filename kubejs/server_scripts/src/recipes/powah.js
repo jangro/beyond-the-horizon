fe8fba75c7b6c2ab3ec1639a21b70744acdc43da
Johannes Lundberg <johalun0@gmail.com>
2024-08-30 07:50:21 -0700

    Adjust aercloud energizing recipe

--- kubejs/server_scripts/src/recipes/powah.js
+++ kubejs/server_scripts/src/recipes/powah.js
@@ -12,7 +12,7 @@ ServerEvents.recipes(event => {
   const ID_PREFIX = 'bth:powah/';
 
   // Aether
-  event.recipes.powah.energizing(['aether:cold_aercloud', 'aether:blue_aercloud', 'aether:golden_aercloud'], 'deep_aether:sterling_aercloud', 10000).id(`${ID_PREFIX}energizing/sterling_aercloud`);
+  event.recipes.powah.energizing(['aether:cold_aercloud', 'aether:blue_aercloud', 'aether:golden_aercloud'], '3x deep_aether:sterling_aercloud', 6000).id(`${ID_PREFIX}energizing/sterling_aercloud`);
 
   // Powah
   event.recipes.powah.energizing(['alexscaves:uranium'], 'powah:uraninite', 3000).id(`${ID_PREFIX}energizing/uraninite`);
