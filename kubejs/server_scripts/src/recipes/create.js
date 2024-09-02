5e2194fad741547e1f8a4a3f94f9c1c8a9c5b1b3
Vaelzan <6418846+Vaelzan@users.noreply.github.com>
2024-09-02 18:54:04 +1000

    Add recipe to make Speed Upgrades in a Create Mixer.

--- kubejs/server_scripts/src/recipes/create.js
+++ kubejs/server_scripts/src/recipes/create.js
@@ -17,6 +17,11 @@ ServerEvents.recipes(event => {
   // Crushing
   event.recipes.create.crushing([Item.of('rats:plastic_waste').withChance(0.5)], 'rats:garbage_pile').id(`${ID_PREFIX}crushing/plastic_waste_from_garbage_pile`);
 
+  // Haunting
+  event.recipes.create.haunting('quark:soul_sandstone', 'minecraft:sandstone').id(`${ID_PREFIX}haunting/soul_sandstone_from_sandstone`);
+  event.recipes.create.haunting('valhelsia_structures:soul_brazier', 'valhelsia_structures:brazier').id(`${ID_PREFIX}haunting/soul_brazier_from_brazier`);
+  event.recipes.create.haunting('valhelsia_structures:unlit_soul_lantern', 'valhelsia_structures:unlit_lantern').id(`${ID_PREFIX}haunting/unlit_soul_lantern_from_unlit_lantern`);
+  
   // Mechanical Crafting
   event.recipes.create.mechanical_crafting('valhelsia_structures:dungeon_door', [
     'CCCC',
@@ -31,17 +36,13 @@ ServerEvents.recipes(event => {
     S: '#forge:plates/steel',
   }).id(`${ID_PREFIX}mechanical_crafting/dungeon_door`);
 
-  // Haunting
-  event.recipes.create.haunting('quark:soul_sandstone', 'minecraft:sandstone').id(`${ID_PREFIX}haunting/soul_sandstone_from_sandstone`);
-  event.recipes.create.haunting('valhelsia_structures:soul_brazier', 'valhelsia_structures:brazier').id(`${ID_PREFIX}haunting/soul_brazier_from_brazier`);
-  event.recipes.create.haunting('valhelsia_structures:unlit_soul_lantern', 'valhelsia_structures:unlit_lantern').id(`${ID_PREFIX}haunting/unlit_soul_lantern_from_unlit_lantern`);
-  
   // Milling
   event.recipes.create.milling(Item.of('rats:plastic_waste').withChance(0.5), 'rats:garbage_pile').id(`${ID_PREFIX}milling/plastic_waste_from_garbage_pile`);
 
   // Mixing
   event.recipes.create.mixing(Fluid.of('minecraft:milk 250'), [Fluid.of('minecraft:water 250'), '#bth:nuts']).heated().id(`${ID_PREFIX}mixing/milk_from_nuts`);
   event.recipes.create.mixing('minecraft:paper', [Fluid.of('minecraft:water 500'), '2x #forge:dusts/wood']).id(`${ID_PREFIX}mixing/paper_from_sawdust`);
+  event.recipes.create.mixing('pneumaticcraft:speed_upgrade', [Fluid.of('pneumaticcraft:lubricant 500'), '2x pneumaticcraft:glycerol', '2x #pneumaticcraft:upgrade_components']).id(`${ID_PREFIX}mixing/speed_upgrade_from_glycerol`);
 
   // Splashing
   event.recipes.create.splashing(Item.of('rats:plastic_waste').withChance(0.5), 'rats:garbage_pile').id(`${ID_PREFIX}splashing/plastic_waste_from_garbage_pile`);
