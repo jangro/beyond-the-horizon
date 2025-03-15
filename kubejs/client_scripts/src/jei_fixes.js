c4331d9fec20ed69f965bac639c2f490e8832b6a
Vaelzan <vaelzan@evilcult.net>
2025-03-15 12:15:30 +1100

    Add Arcane Anvil to JEI.

--- kubejs/client_scripts/src/jei_fixes.js
+++ kubejs/client_scripts/src/jei_fixes.js
@@ -10,6 +10,7 @@
  */
 JEIEvents.addItems(event => {
   event.add('irons_spellbooks:alchemist_cauldron');
+  event.add('irons_spellbooks:arcane_anvil');
   event.add('irons_spellbooks:firefly_jar');
   event.add('irons_spellbooks:inscription_table');
   event.add('irons_spellbooks:scroll_forge');
