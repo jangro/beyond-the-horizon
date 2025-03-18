a79d1f8b66e0b48c899853828df440d0dfc9a04f
Johannes Lundberg <johalun0@gmail.com>
2025-03-17 18:37:53 -0700

    Use KubeJS to set gamerules instead of Global Gamerules mod

---
+++ kubejs/server_scripts/src/server_loaded.js
@@ -0,0 +1,19 @@
+// priority: 5                  
+
+/**
+ * @file Server Loaded Script
+ * 
+ * Useful for setting things like modded gamerules, or other server-wide settings.
+ */
+
+/**
+ * Server loaded event handler
+ */
+ServerEvents.loaded(event => {
+  if (!event.server.persistentData.contains('gamerules_v1')){
+    console.log('Setting gamerules v1');
+    event.server.persistentData.put('gamerules_v1', true);
+    event.server.runCommandSilent('/gamerule keepWallet true');
+    event.server.runCommandSilent('/gamerule artifacts.rootedBoots.enabled false');
+  } 
+});
