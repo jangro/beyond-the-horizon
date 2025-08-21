c02bfb65f22bbc02d21f24ed161ea1c9e8a76cc1
Johannes Lundberg <johalun0@gmail.com>
2025-08-21 10:52:21 -0700

    Add Java version check to startserver.sh

--- overrides/startserver.sh
+++ overrides/startserver.sh
@@ -4,11 +4,33 @@
 
 # Edit the below values to change JVM Arguments or Allocated RAM for the server.
 JAVA_PATH="java"
+JAVA_VERSIONS="17 21"
 ALLOCATED_RAM="8G"
 JVM_ARGUMENTS="-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch"
 
 # Make sure this matches the Forge version of the server if you update.
 FORGE_VERSION="1.20.1-47.4.6"
+
+# Verify that java version is supported
+JAVA_VERSION=$(${JAVA_PATH} -version 2>&1 | head -n1 | sed -n 's/.*"\([0-9][0-9]*\)\..*/\1/p')
+if [ -z "${JAVA_VERSION}" ]; then
+    # Try alternative format for newer Java versions (e.g., "java 17.0.1")
+    JAVA_VERSION=$(${JAVA_PATH} -version 2>&1 | head -n1 | sed -n 's/.* \([0-9][0-9]*\)\..*/\1/p')
+fi
+
+# Check if JAVA_VERSION is in the list of supported versions
+VERSION_FOUND=false
+for supported_version in ${JAVA_VERSIONS}; do
+    if [ "${JAVA_VERSION}" = "${supported_version}" ]; then
+        VERSION_FOUND=true
+        break
+    fi
+done
+
+if [ "${VERSION_FOUND}" = "false" ]; then
+    echo "Java ${JAVA_VERSIONS} is required to run this server. Found Java ${JAVA_VERSION:-unknown}."
+    exit 1
+fi
 
 # Install Minecraft Forge if it isn't already installed.
 FILE=./libraries/net/minecraftforge/forge/${FORGE_VERSION}
@@ -45,4 +67,5 @@ fi
 echo "Starting Beyond The Horizon Server."
 ${JAVA_PATH} -Xms${ALLOCATED_RAM} -Xmx${ALLOCATED_RAM} ${JVM_ARGUMENTS} @libraries/net/minecraftforge/forge/${FORGE_VERSION}/unix_args.txt nogui
 
-read -p "Press enter to continue."
+printf "Press enter to continue."
+read _
