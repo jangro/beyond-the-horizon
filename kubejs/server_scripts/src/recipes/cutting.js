a2b81c6f88536bb0775262197688398c9cf9bb85
Johannes Lundberg <johalun0@gmail.com>
2025-08-26 10:37:54 -0700

    Add cutting recipes for archwood

--- kubejs/server_scripts/src/recipes/cutting.js
+++ kubejs/server_scripts/src/recipes/cutting.js
@@ -4,34 +4,41 @@
  * @file Cutting board recipe additions for Beyond the Horizon.
  */
 
-/**
- * Cutting Recipe Event Handler
- */
-ServerEvents.recipes(event => {
-  const ID_PREFIX = 'bth:cutting/';
-
-
+function addLogStrippingRecipe(event, inputItem, outputItem, id) {
   event.custom({
     "type": "farmersdelight:cutting",
     "ingredients": [
-      {
-        "item": "eidolon:illwood_log"
-      }
+      { "item": inputItem }
     ],
     "result": [
-      {
-        "item": "eidolon:stripped_illwood_log"
-      },
-      {
-        "item": "farmersdelight:tree_bark"
-      }
+      { "item": outputItem },
+      { "item": 'farmersdelight:tree_bark' }
     ],
     "sound": "eidolon:item.axe.strip",
     "tool": {
       "type": "farmersdelight:tool_action",
       "action": "axe_strip"
     }
-  }).id(`${ID_PREFIX}illwood_log`);
+  }).id(id);
+}
+
+/**
+ * Cutting Recipe Event Handler
+ */
+ServerEvents.recipes(event => {
+  const ID_PREFIX = 'bth:cutting/';
+
+  addLogStrippingRecipe(event, "ars_elemental:yellow_archwood_log", "ars_elemental:stripped_yellow_archwood_log", `${ID_PREFIX}yellow_archwood_log`);
+  addLogStrippingRecipe(event, "ars_elemental:yellow_archwood", "ars_elemental:stripped_yellow_archwood", `${ID_PREFIX}yellow_archwood`);
+  addLogStrippingRecipe(event, "ars_nouveau:blue_archwood_log", "ars_nouveau:stripped_blue_archwood_log", `${ID_PREFIX}blue_archwood_log`);
+  addLogStrippingRecipe(event, "ars_nouveau:blue_archwood_wood", "ars_nouveau:stripped_blue_archwood_wood", `${ID_PREFIX}blue_archwood_wood`);
+  addLogStrippingRecipe(event, "ars_nouveau:green_archwood_log", "ars_nouveau:stripped_green_archwood_log", `${ID_PREFIX}green_archwood_log`);
+  addLogStrippingRecipe(event, "ars_nouveau:green_archwood_wood", "ars_nouveau:stripped_green_archwood_wood", `${ID_PREFIX}green_archwood_wood`);
+  addLogStrippingRecipe(event, "ars_nouveau:purple_archwood_log", "ars_nouveau:stripped_purple_archwood_log", `${ID_PREFIX}purple_archwood_log`);
+  addLogStrippingRecipe(event, "ars_nouveau:purple_archwood_wood", "ars_nouveau:stripped_purple_archwood_wood", `${ID_PREFIX}purple_archwood_wood`);
+  addLogStrippingRecipe(event, "ars_nouveau:red_archwood_log", "ars_nouveau:stripped_red_archwood_log", `${ID_PREFIX}red_archwood_log`);
+  addLogStrippingRecipe(event, "ars_nouveau:red_archwood_wood", "ars_nouveau:stripped_red_archwood_wood", `${ID_PREFIX}red_archwood_wood`);
+  addLogStrippingRecipe(event, "eidolon:illwood_log", "eidolon:stripped_illwood_log", `${ID_PREFIX}illwood_log`);
 
   event.custom({
     "type": "farmersdelight:cutting",
